c318a981c585736f16f9daa66939cd91ca4a1db5
GhettoTurtel <dtansykov@gmail.com>
2024-05-02 01:56:59 +0500

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Damir Tansykov on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
